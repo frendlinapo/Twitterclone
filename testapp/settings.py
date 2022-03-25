1aa842ccd8c11c97296b5a37e1b9c1abc5e3797e
Frendli Napo <frendlinapo@gmail.com>
2022-03-24 22:22:47 -0400

    added files

--- testapp/settings.py
+++ testapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-jh+hn1h7zsqk8ns(jh4lg)g7v73smr9d=mm62yy+z!8uhfr6^_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pictures'
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
